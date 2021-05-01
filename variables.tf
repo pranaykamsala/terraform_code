9e190109c5fc35aadbc2225864933b260e4231d2
pranay <pranaykamsala5@gmail.com>
2021-05-01 13:03:53 +0530

    terrfrom intial version

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+variable "amis" {
+    description = "AMIs by region"
+    default = {
+        ap-south-1 = "ami-045e6fa7127ab1ac4" # ubuntu 14.04 LTS
+		ap-south-1 = "ami-045e6fa7127ab1ac4" # ubuntu 14.04 LTS
+		ap-south-1 = "ami-045e6fa7127ab1ac4" # ubuntu 14.04 LTS
+		ap-south-1 = "ami-045e6fa7127ab1ac4" # ubuntu 14.04 LTS
+    }
+}
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "IGW_name" {}
+variable "key_name" {}
+variable "public_subnet1_cidr" {}
+variable "public_subnet2_cidr" {}
+variable "public_subnet3_cidr" {}
+variable "private_subnet_cidr" {}
+variable "public_subnet1_name" {}
+variable "public_subnet2_name" {}
+variable "public_subnet3_name" {}
+variable "private_subnet_name" {}
+variable Main_Routing_Table {}
+variable "azs" {
+  description = "Run the EC2 Instances in these Availability Zones"
+  type = "list"
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+variable "environment" { default = "dev" }
+variable "instance_type" {
+  type = "map"
+  default = {
+    dev = "t2.nano"
+    test = "t2.micro"
+    prod = "t2.medium"
+    }
+}
+
